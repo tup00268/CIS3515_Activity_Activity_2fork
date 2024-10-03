2535233f9b885750c81eaf3135300e36f22a04d5
Roy Shimon <tup00268@temple.edu>
2024-10-03 15:18:52 -0400

    completed activity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,10 +1,16 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
+
+const val MESSAGE_KEY = "message"
+const val RESULT_KEY = "reply_message"
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
@@ -14,12 +20,24 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_display)
-
-        lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
-        textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
-
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            it.data?.apply {
+                lyricsDisplayTextView.textSize = getFloatExtra(RESULT_KEY, 22f)
+            }
+        }
     }
-}
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_display)
+
+            lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
+            textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+            textSizeSelectorButton.setOnClickListener{
+                launcher.launch(Intent(this@DisplayActivity, TextSizeActivity::class.java))
+            }
+
+
+        }
+    }
